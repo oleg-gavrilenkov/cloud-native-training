6c8e3ac00e50ac15361d7d7aaff5df4bedbabaa1
Oleh Havrylenkov <oleg_gavrilenkov@epam.com>
2024-01-09 16:16:01 +0100

    add task02

---
+++ task02/jsrc/main/java/com/task02/HelloWorldUrl.java
@@ -0,0 +1,33 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world_url",
+	roleName = "hello_world_url-role",
+	isPublishVersion = true
+)
+@LambdaUrlConfig(
+authType = AuthType.NONE,
+invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorldUrl implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("statusCode", 200);
+		body.put("message", "Hello from Lambda");
+
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("body", body);
+		return resultMap;
+	}
+}
